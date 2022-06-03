92c2fb28263f960ae6ad594dc7f5f7d864964ffa
FrancoRogel_Johan <johan.franco147@gmail.com>
2022-06-02 22:19:20 -0500

    prueba 2

--- src/Admin.js
+++ src/Admin.js
@@ -83,7 +83,7 @@ export default function Admin() {
         var arr = [];
         for(var key in json){
             var temp = json[key];
-            var st1 = `Producto: ${temp[0]}\nPrecio: ${temp[1]}\nCantidad: ${temp[2]}`;
+            var st1 = `Producto: ${temp[0]}\nPrecio: ${temp[1]}\nCantidad: ${temp[2]}<br></br>`;
 
             arr.push(st1);
         }
